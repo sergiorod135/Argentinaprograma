04cd1101de59078d1838b5b02c52efccf5b77f41
sergioorod135 <sergiorod135@gmail.com>
2023-05-28 14:06:50 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,4 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
---
+++ promedio3a.js
@@ -0,0 +1,5 @@
+let x = 5;
+let y = 7;
+let z = 9;
+let promedio = ( x + y + z) / 3;
+console.log(promedio);
